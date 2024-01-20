65c8647401f8d851297e5fe159585bb9047f3ba0
Bruno <brunno_1211@hotmail.com>
2024-01-20 01:01:23 -0300

    alterações

---
+++ projetoXYZ/Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projetoXYZ.Context;
+using projetoXYZ.Models;
+using System.Web;
+
+namespace projetoXYZ.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetData()
+        {
+            try
+            {
+                using (_context)
+                {
+                    List<Employee> empList = _context.Employees.ToList<Employee>();
+                    return Json(new { data = empList, success = true });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { message = ex.Message, success = false });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            return View(new Employee());
+        }
+
+        [HttpPost]
+        public IActionResult AddOrEdit()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
--- projetoXYZ/Program.cs
+++ projetoXYZ/Program.cs
@@ -3,19 +3,22 @@ using projetoXYZ.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options => {
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
     options.UseSqlServer(builder.Configuration.GetConnectionString("Projeto"));
 });
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    //app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
 
@@ -23,8 +26,15 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseEndpoints(endpoints =>
+{
+    //endpoints.MapAreaControllerRoute("areas", "IPODB", "{area}/{controller=MainMenu}/{action=Index}/{id?}");
+    //endpoints.MapControllerRoute("default", "{controller=Employee}/{action=Index}/{id?}");
+    endpoints.MapControllers();
+});
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Employee}/{action=Index}/{id?}");
 
 app.Run();
