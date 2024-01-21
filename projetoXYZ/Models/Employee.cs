e7914255173f9cc6315a2e4a63d3063c2c7ac43c
Bruno <brunno_1211@hotmail.com>
2024-01-21 16:14:20 -0300

    aaa

--- projetoXYZ/Models/Employee.cs
+++ projetoXYZ/Models/Employee.cs
@@ -12,6 +12,7 @@ namespace projetoXYZ.Models
         public string Name { get; set; } = string.Empty;
         [Required(ErrorMessage = "This field is Required")]
         public string Position { get; set; } = string.Empty;
+        [Required(ErrorMessage = "This field is Required")]
         public string Office { get; set; } = string.Empty;
         public int Age { get; set; }
         public int Salary { get; set; }
