836a1138d9afb80081db593e91d2b5207ea460c8
Bruno <brunno_1211@hotmail.com>
2024-02-13 21:06:29 -0300

    inclusão de data com tipo string

--- projetoXYZ/Models/Employee.cs
+++ projetoXYZ/Models/Employee.cs
@@ -16,10 +16,6 @@ namespace projetoXYZ.Models
         public string Office { get; set; } = string.Empty;
         public int Age { get; set; }
         public int Salary { get; set; }
-
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0: yyyy-MM-dd}")]
-        public DateTime Date { get; set; } = new DateTime(2019, 01, 01);
+        public string DateString { get; set; } = string.Format("{0: yyyy-MM-dd}", "2019-01-01");
     }
 }
